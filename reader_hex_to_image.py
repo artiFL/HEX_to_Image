253a2a78dc613459bfd4a2605efa216b6e04596e
artiFL <artifl3221@gmail.com>
2022-08-23 00:17:29 +0700

    Initial commit

---
+++ reader_hex_to_image.py
@@ -0,0 +1,59 @@
+import numpy
+import matplotlib as plt
+from PIL import Image
+from os import makedirs
+from os.path import join as osjoin, dirname
+import time
+
+height = 100
+width = 100
+
+cadr_x_start = 0
+cadr_y_start = 0
+
+
+cadr_x_end = width
+cadr_y_end = height
+
+
+step = width * height * 2
+
+#with open('C:/Users/artif/OneDrive/Рабочий стол/sdhrg.hex', 'rb') as fp:
+with open('C:/Users/artif/OneDrive/Рабочий стол/fl_displey.hex', 'rb') as fp:
+    hex_list = fp.read()
+    
+
+hex_list = ['{:02X}'.format(b) for b in hex_list]
+
+def twosComplement_hex(hexval):
+    bits = 16
+    val = int(hexval, bits)
+    if val & (1 << (bits-1)):
+        val -= 1 << bits
+    return val
+
+img = Image.new('RGB', (240, 240))
+
+index = 0xb0000
+
+while True:
+
+    for y in range(0, height):
+
+        for x in range(0, width):
+
+            a = twosComplement_hex(hex_list[index])
+            b = twosComplement_hex(hex_list[index + 1])
+
+            img.putpixel((x,y), (b & 248, 36 * (b & 7) + (a & 192) // 8, 4 * (a & 63)))
+            index += 2
+
+    width += 2
+    height += 2
+    print(width)
+    index = 0xb0000
+    
+    #time.sleep(1)
+    #img.save(osjoin(f"{width}") + ".PNG")
+
+    img.show()
