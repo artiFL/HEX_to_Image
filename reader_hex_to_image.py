9fb637146711c38ed0ca9aaee1ceeecc9d109735
flegler.a <flegler.a@future-tech.ru>
2022-08-24 17:55:37 +0700

    Add commentaries

--- reader_hex_to_image.py
+++ reader_hex_to_image.py
@@ -1,31 +1,64 @@
 from PIL import Image
 from os.path import join as osjoin, dirname
 
-height = 100
-width = 100
+'''
+Режим перебора ширины изображения нужен для подбора действующей жирины изображения (выравнивания)
+т.к изначальная ширина изображения не известна а баты рассположены последовательно друг за гругом необходимо знать на каком значении заканчивается 1 горизонтальная строка пикселей
+в ином случае даже при сдвиге на 1 пиксель изображение ломается и становится нечитаемым
 
-cadr_x_start = 0
-cadr_y_start = 0
+Если исспользуется режим перебора ширины изображения 
+значение "width" необходимо поставить меньше переменной "width_end"
+'''
 
-step = width * height * 2
+start_address = 0x00000                                 #Адресс начала чтения Hex файла
 
-with open('C:/Users/artif/OneDrive/Рабочий стол/fl_displey.hex', 'rb') as fp:
-    hex_list = fp.read()
+height = 320                                            #Высота получаемого изображения 
+width = 400                                              
+step_iteration =  1                                     #Шаг итерации ширины
+width_end = 480                                         #Ширина получаемого изображения
+
+flag_pars = 1                                           #Флаг метода если равен 0 то скрипт выводит 1 изображение если равен 1 то выводит серию изображений с перебором ширины и высоты изображения 
+
+
+
+Path = 'C:/Users/flegler.a/Desktop/fl_displey.hex'      #Указывается путь до читаемого файла (обратить внимание что при указании путей исспользуется '/' вместо '\')
+
+with open(Path, 'rb') as fp:                            #Открывтие файла в bin режиме
+    hex_list = fp.read()                                #Чтение всего файла
     
-hex_list = ['{:02X}'.format(b) for b in hex_list]
+hex_list = ['{:02X}'.format(b) for b in hex_list]       #Преобразование массива байт в список
 
-def twosComplement_hex(hexval):
+def twosComplement_hex(hexval):                         #Функия преобразования полученой каши в удобочитаемый вид
     bits = 16
     val = int(hexval, bits)
     if val & (1 << (bits-1)):
         val -= 1 << bits
     return val
 
-img = Image.new('RGB', (240, 240))
+img = Image.new('RGB', (width_end, height))                      #создание объекта изображения
 
-index = 0xb0000
+index = start_address                                   #задание итерируемого стартового адреса
 
-while True:
+if flag_pars == 1:
+    while True:
+        for y in range(0, height):
+            for x in range(0, width_end):
+
+                a = twosComplement_hex(hex_list[index])                  #чтение 2х байт для 1го пикселя                   
+                b = twosComplement_hex(hex_list[index + 1])
+
+                img.putpixel((x,y), (b & 248, 36 * (b & 7) + (a & 192) // 8, 4 * (a & 63))) #конвертация uint16_t в RGB565 
+                index += 2
+
+        width += step_iteration                  
+
+        print(width)
+
+        index = start_address
+
+        img.show()
+
+else:
 
     for y in range(0, height):
 
@@ -39,9 +72,9 @@ while True:
 
     width += 2
     height += 2
-    
+
     print(width)
 
-    index = 0xb0000
+    index = start_address
 
     img.show()
